78af648d2c43deba08328a6b743688e5e2f47a7a
sagorforu <freelancersagor18@gmail.com>
2023-05-19 16:11:30 +0600

    post woring successfully

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('toysDB').collection('toys');
+
+    app.post('/addToy', async(req, res) => {
+      const toy = req.body;
+      const result = await toysCollection.insertOne(toy);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
